ca84018ed290499f46c55d3520789d29fed19e6a
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-04-27 22:02:08 +0300

    Initial Commit

---
+++ Diya/DiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiyaApp.swift
+//  Diya
+//
+//  Created by Gleb Renkas on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
